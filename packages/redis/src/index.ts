2e48b2c1197c0a7ff18a44cd21d7a2cb6a7cad7e
Adam Horvath <vorhdam@gmail.com>
2026-07-12 18:43:58 +0200

    Added redis for caching and ratelimiting

---
+++ packages/redis/src/index.ts
@@ -0,0 +1,21 @@
+import { redis, RedisClient } from "bun";
+
+const redisClientSingleton = () => {
+  return process.env.REDIS_URL ? new RedisClient(process.env.REDIS_URL) : redis;
+};
+
+declare const globalThis: {
+    redisGlobal: RedisClient;
+  } & typeof global;
+
+/**
+ * ## Redis Client
+ * @description The Redis Client that is used for caching and ratelimiting
+ */
+const client = globalThis.redisGlobal ?? redisClientSingleton();
+
+if (!client.connected) await client.connect();
+
+if (process.env.NODE_ENV !== "production") globalThis.redisGlobal = client;
+
+export default client;
